57eef3d5ce097335083d654920fc124c8ab36b5a
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2021-08-31 20:37:19 +0800

    fix linting

--- app_folder_images.py
+++ app_folder_images.py
@@ -8,7 +8,7 @@ from ScaledYOLOv4.scaled_yolov4 import Scaled_YOLOV4
 
 @st.cache(allow_output_mutation=True)
 def initialize_od():
-    yolov4 = Scaled_YOLOV4( 
+    return Scaled_YOLOV4( 
         bgr=True,
         gpu_device=0,
         model_image_size=608,
@@ -17,47 +17,45 @@ def initialize_od():
         same_size=True
     )
 
-    return yolov4
 
 @st.cache
 def process_image(od, image, confidence_threshold, nms_threshold):
     od.thresh = confidence_threshold
     od.nms_thresh = nms_threshold
-    detections = od.detect_get_box_in([image], box_format='ltrb', classes=None)[0]
+    return od.detect_get_box_in([image], box_format='ltrb', classes=None)[0]
 
-    return detections
 
 @st.cache
 def annotate_image(image, detections):
     draw_frame = image.copy()
     for det in detections:
         bb, score, det_class = det 
-        l,t,r,b = bb
-        cv2.rectangle(draw_frame, (l,t), (r,b), (255,0,0), 2)
-        label = f"{det_class}: {score:.2f}"
-        cv2.putText(draw_frame, label, (l+5, t+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+        l, t, r, b = bb
+        cv2.rectangle(draw_frame, (l, t), (r, b), (255, 0, 0), 2)
+        label = f'{det_class}: {score:.2f}'
+        cv2.putText(draw_frame, label, (l+5, t+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     
     return draw_frame
 
 
-st.set_page_config(layout="wide")
-st.title("Object detection with ScaledYOLOv4 (Folder of images)")
-images_folder = st.text_input("Folder path containing images")
-confidence_threshold = st.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.05)
-nms_threshold = st.slider("NMS threshold", 0.0, 1.0, 0.5, 0.05)
-num_cols = st.slider("Number of display columns", 1, 5, 2, 1)
+st.set_page_config(layout='wide')
+st.title('Object detection with ScaledYOLOv4 (Folder of images)')
+images_folder = st.text_input('Folder path containing images')
+confidence_threshold = st.slider('Confidence threshold', 0.0, 1.0, 0.5, 0.05)
+nms_threshold = st.slider('NMS threshold', 0.0, 1.0, 0.5, 0.05)
+num_cols = st.slider('Number of display columns', 1, 5, 2, 1)
 
 od = initialize_od()
 
 images_folder = str(images_folder) or None
 
-suffixes = [".png", ".jpg", ".jpeg"]
+suffixes = ['.png', '.jpg', '.jpeg']
 if images_folder is not None:
     if not Path(images_folder).is_dir():
-        st.error("Folder does not exist!")
+        st.error('Folder does not exist!')
     else:
         all_images = []
-        for image_path in Path(images_folder).glob("*"):
+        for image_path in Path(images_folder).glob('*'):
             if image_path.suffix in suffixes:
                 image = cv2.imread(str(image_path))
 
@@ -67,7 +65,7 @@ if images_folder is not None:
                 all_images.append(img)
 
         for i in range(0, len(all_images), num_cols):
-            cols = st.beta_columns(num_cols)
+            cols = st.columns(num_cols)
             images = all_images[i:i+num_cols]
             for j in range(len(images)):
-                cols[j].image(images[j], channels="BGR")
+                cols[j].image(images[j], channels='BGR')
--- app_single_image.py
+++ app_single_image.py
@@ -7,7 +7,7 @@ from ScaledYOLOv4.scaled_yolov4 import Scaled_YOLOV4
 
 @st.cache(allow_output_mutation=True)
 def initialize_od():
-    yolov4 = Scaled_YOLOV4( 
+    return Scaled_YOLOV4( 
         bgr=True,
         gpu_device=0,
         model_image_size=608,
@@ -16,15 +16,13 @@ def initialize_od():
         same_size=True
     )
 
-    return yolov4
 
 @st.cache
 def process_image(od, image, confidence_threshold, nms_threshold):
     od.thresh = confidence_threshold
     od.nms_thresh = nms_threshold
-    detections = od.detect_get_box_in([image], box_format='ltrb', classes=None)[0]
+    return od.detect_get_box_in([image], box_format='ltrb', classes=None)[0]
 
-    return detections
 
 @st.cache
 def annotate_image(image, detections):
@@ -32,20 +30,20 @@ def annotate_image(image, detections):
     labels = []
     for det in detections:
         bb, score, det_class = det 
-        l,t,r,b = bb
-        cv2.rectangle(draw_frame, (l,t), (r,b), (255,0,0), 2)
-        label = f"{det_class}: {score:.2f}"
-        cv2.putText(draw_frame, label, (l+5, t+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+        l, t, r, b = bb
+        cv2.rectangle(draw_frame, (l, t), (r, b), (255, 0, 0), 2)
+        label = f'{det_class}: {score:.2f}'
+        cv2.putText(draw_frame, label, (l+5, t+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         labels.append(label)
     
     return draw_frame, labels
 
 
-st.set_page_config(layout="wide")
-st.title("Object detection with ScaledYOLOv4")
-img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-confidence_threshold = st.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.05)
-nms_threshold = st.slider("NMS threshold", 0.0, 1.0, 0.5, 0.05)
+st.set_page_config(layout='wide')
+st.title('Object detection with ScaledYOLOv4')
+img_file_buffer = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
+confidence_threshold = st.slider('Confidence threshold', 0.0, 1.0, 0.5, 0.05)
+nms_threshold = st.slider('NMS threshold', 0.0, 1.0, 0.5, 0.05)
 
 od = initialize_od()
 
@@ -57,7 +55,7 @@ if img_file_buffer is not None:
     image, labels = annotate_image(image, detections)
 
     st.image(
-        image, caption=f"Processed image", use_column_width=True, channels="BGR"
+        image, caption=f'Processed image', use_column_width=True, channels='BGR'
     )
 
     st.write(labels)
